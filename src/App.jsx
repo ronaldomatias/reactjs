77574b9124a02fdd5692e15efa0ea52c379e06fd
Ronaldo Matias <ronaldomatias49@hotmail.com>
2022-08-06 22:55:12 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Welcome from './components/Welcome';
+
+function App() {
+  
+  return (<>
+    <h1>Definindo Componentes</h1>
+    <hr/>
+    <Welcome />
+    <p>...</p>
+    <p>...</p>
+    <p>...</p>
+    <Welcome />
+
+  </>)
+}
+
+export default App
---
+++ src/components/Welcome.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Welcome extends React.Component {
+  render() {
+    return <h1>Olá Mundo! Seja bem vindo!</h1> 
+  }
+} 
+
+export default Welcome
+
+
